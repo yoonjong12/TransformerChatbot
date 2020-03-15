fc6f30257a56af3f4812c62ce3408e431758e75f
yoonjong12 <jhku11@naver.com>
2020-03-15 10:50:12 +0900

    no message

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+weights_dir = os.path.normpath('./weight/')
+vocab_dir = os.path.normpath('./tokenizer/')
+
+# tensorflow tokenizer
+vocab_name = "tokenizer_data.json"
+
+# Keras model
+weights_name = "my_keras_model.h5"
---
+++ main.py
@@ -0,0 +1,39 @@
+from module import Transformer
+from config import *
+from konlpy.tag import Okt
+import tensorflow as tf
+
+def startVerOne():
+    config = tf.compat.v1.ConfigProto()
+    config.gpu_options.allow_growth = True
+    tf.compat.v1.enable_eager_execution(config=config)
+startVerOne()
+
+if tf.__version__[0] == '1':
+    print('Using TF ver.1')
+    startVerOne()
+else:
+    print('Using TF 2.0')
+
+
+t = Okt()
+
+weights = os.path.join(weights_dir, weights_name)
+tokenizer = os.path.join(vocab_dir, vocab_name)
+
+model = Transformer.modelLoad(weights, MAX_LENGTH=30)
+tokenizer = Transformer.tokenizerLoad(tokenizer)
+
+
+
+# 예측
+
+while True:
+    sentence = input('Q : ')
+    if sentence == 'quit':
+        break
+    else:
+        prediction = Transformer.evaluate(model, tokenizer, sentence, t, anl_type='Konlpy_tokens', MAX_LENGTH=30)
+        new_text = ' '.join(prediction)
+
+        print(f'A : {new_text}')# 예측
